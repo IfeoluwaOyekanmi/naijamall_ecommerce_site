439b8d39ad47285829a6fd012fbb4887c5553b72
IfeoluwaOyekanmi <ifeoluwagodishere@gmail.com>
2024-10-26 03:42:45 +0100

    data import script done

---
+++ backend/data/products.js
@@ -0,0 +1,52 @@
+const products = [
+  {
+    name: "CottonSeed",
+    imageUrl: 'https://drive.google.com/file/d/1VUXhjNxyGl5-obfFNbr2JHMWqZ8pG_6F/view?usp=drivesdk',
+    description:
+    "This is cotton seed of different cotton seeds planted in different places in different parts of the country Nigeria. The seed is distributed in different parts of the country where the seed is distributed in different parts of the country using supply chain logistics to Japan, Turkiye, India, France and, Australia, Portuguese, Tunisia and, Scotland",  
+    price: 1000,
+    countInstock: 20
+  },
+  {
+    name: "Leatherbags",
+    imageUrl: 'https://drive.google.com/file/d/1VUXyA8-O0o-DxCvCt4hknjnBuJKn7NMJ/view?usp=drivesdk',
+    description: 
+    "StyleSee The 10 Nigerian-Owned Handbag Brands You will Want To Splurge OnAvatar photoPublished 2 years ago  on July 25, 2022By BellaNaija Style Before now, made-in-Nigeria bags were probably looked down upon in terms of quality",
+    price: 1000,
+    countInstock: 20
+  },
+  {
+    name: "Aluminium",
+    imageUrl: 'https://drive.google.com/file/d/1VTeOLia7C0dZ4TgtvqZeqzes3y952HrL/view?usp=drivesdk',
+    description:
+    "This is cotton seed of different cotton seeds planted in different places in different parts of the country Nigeria. The seed is distributed in different parts of the country where the seed is distributed in different parts of the country using supply chain logistics to Japan, Turkiye, India, France and, Australia, Portuguese, Tunisia and, Scotland",  
+    price: 1000,
+    countInstock: 20
+  },
+  {
+    name: "Honey",
+    imageUrl: 'https://drive.google.com/file/d/1VT7TrXIJ-N6SILsYO2mrnw0Z2_4aeaQv/view?usp=drivesdk',
+    description:
+    "This is cotton seed of different cotton seeds planted in different places in different parts of the country Nigeria. The seed is distributed in different parts of the country where the seed is distributed in different parts of the country using supply chain logistics to Japan, Turkiye, India, France and, Australia, Portuguese, Tunisia and, Scotland",  
+    price: 1000,
+    countInstock: 20
+  },
+  {
+    name: "Charcoal",
+    imageUrl: 'https://drive.google.com/file/d/1VWuOKa8vZOKZQPqrRNhbl391HSh2fCPa/view?usp=drivesdk',
+   description:
+    "This is cotton seed of different cotton seeds planted in different places in different parts of the country Nigeria. The seed is distributed in different parts of the country where the seed is distributed in different parts of the country using supply chain logistics to Japan, Turkiye, India, France and, Australia, Portuguese, Tunisia and, Scotland",  
+    price: 1000,
+    countInstock: 20
+  },
+  {
+    name: "Calabashes",
+    imageUrl: 'https://drive.google.com/file/d/1VPnPulnY0jNxdTzY8ixITkM0k2TVttic/view?usp=drivesdk',
+    description:
+    "This is cotton seed of different cotton seeds planted in different places in different parts of the country Nigeria. The seed is distributed in different parts of the country where the seed is distributed in different parts of the country using supply chain logistics to Japan, Turkiye, India, France and, Australia, Portuguese, Tunisia and, Scotland",  
+    price: 1000,
+    countInstock: 20
+  },
+];
+
+module.exports =products;
--- backend/server.js
+++ backend/server.js
@@ -7,4 +7,4 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log('server running on port ${PORT}'));
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
