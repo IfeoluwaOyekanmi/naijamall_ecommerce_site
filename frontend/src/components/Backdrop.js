2f6540d1b7b5f3a34d9f9da4188b5ac1da3fad39
IfeoluwaOyekanmi <ifeoluwagodishere@gmail.com>
2024-10-31 06:55:28 +0100

    backdrop styling commenced

---
+++ frontend/src/components/Backdrop.js
@@ -0,0 +1,10 @@
+import './Backdrop.css';
+const Backdrop = () => {
+  return (
+    <div className="">
+
+    </div>
+  )
+}
+
+export default Backdrop;
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
             <Link to='/cart' className='cart__link'>
             <i className='fas fa-shopping-cart'></i>
              <span>Cart</span>
-             <span className="cartlogo__badge"> 0 </span>
+             <span className="cartlogo_badge"> 0 </span>
             </Link> 
           </li>
           <li>
